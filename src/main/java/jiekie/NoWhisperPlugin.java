6e98ced8a0c4e288445034c0c61283e606f4b2d8
BAEKJlHYE <bjiekie@gmail.com>
2025-04-21 17:37:58 +0900

    add copyright

--- src/main/java/jiekie/NoWhisperPlugin.java
+++ src/main/java/jiekie/NoWhisperPlugin.java
@@ -8,6 +8,9 @@ public final class NoWhisperPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
         getServer().getPluginManager().registerEvents(new CommandEvent(this), this);
+
+        getLogger().info("귓속말 금지 플러그인 by Jiekie");
+        getLogger().info("Copyright © 2025 Jiekie. All rights reserved.");
     }
 
     @Override
